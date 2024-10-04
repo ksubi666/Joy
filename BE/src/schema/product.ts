b97b193eeffccba3f46ce62d0e5bcac779b2e804
Sureneee <soyolsurenbs@gmail.com>
2024-10-04 15:39:00 +0800

    [BE] product schema

---
+++ BE/src/schema/product.ts
@@ -0,0 +1,35 @@
+import { Schema, model } from "mongoose";
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"]
+  },
+  image: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  price: {
+    type: Number,
+    required: [true, "Price is required"]
+  },
+  discount: {
+    type: Number,
+    default: 0
+  },
+  categoryId: {
+    type: Schema.ObjectId,
+    ref: "category",
+    required: [true, "Category id required"]
+  },
+  reviewId: {
+    type: Schema.ObjectId,
+    ref: "review"
+  }
+
+})
+
+export const ProductModel = model("product", productSchema);
+
