60a7fa7cb8c610eecd358fd35a6bde917339624b
ksubi666 <Ksubi666@gmail.com>
2024-10-07 09:52:31 +0800

    [BE] user update and delete

--- BE/src/controller/user.ts
+++ BE/src/controller/user.ts
@@ -1,6 +1,7 @@
 import { UserModel } from 'src/schema/user';
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export const createUser = async (req: Request, res: Response) => {
   const { name, email, password, phone, role } = req.body;
@@ -37,6 +38,41 @@ export const getUser = async (req: Request, res: Response) => {
 export const getUsers = async (res: Response) => {
   try {
     const response = await UserModel.find();
+    return res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+};
+
+export const UserUpdate = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const { name, email, password, phone, role } = req.body;
+
+  try {
+    const response = await UserModel.findByIdAndUpdate(id, {
+      name,
+      email,
+      password,
+      phone,
+      role,
+    });
+    const privateKey = process.env.JWT_PRIVATE_KEY;
+    const token = jwt.sign({ ...response }, privateKey);
+
+    return res.status(200).cookie('token', token).end();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+};
+
+export const userDelete = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const response = await UserModel.findByIdAndDelete(id);
     return res.status(200).json(response);
   } catch (error) {
     console.log(error);
