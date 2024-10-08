98cd2bcd3f6832e546306898efd14dd2140b8391
ksubi666 <Ksubi666@gmail.com>
2024-10-09 00:53:27 +0800

    [FE] navbar only ui

---
+++ FE/src/app/icons/SearchIcon.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const SearchIcon = () => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M7.75 13.5C10.9256 13.5 13.5 10.9256 13.5 7.75C13.5 4.57436 10.9256 2 7.75 2C4.57436 2 2 4.57436 2 7.75C2 10.9256 4.57436 13.5 7.75 13.5Z"
+        stroke="#111A2B"
+      />
+      <path d="M17 17L12 12" stroke="#111A2B" />
+    </svg>
+  );
+};
+
+export default SearchIcon;
