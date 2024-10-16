8a158766c215fb54fd4ea0b29dea1fe36491aba9
Sureneee <soyolsurenbs@gmail.com>
2024-10-16 16:45:18 +0800

    [FE] signup page only ui

--- FE/src/app/page.tsx
+++ FE/src/app/page.tsx
@@ -1,22 +1,24 @@
-// import Subcategory from '@/components/Subcategory';
-// import MainPage from '../components/MainPage';
-// import Card from '@/components/Card';
+import Subcategory from '@/components/Subcategory';
+import MainPage from '../components/MainPage';
+import Card from '@/components/Card';
 import ProductDetail from '@/components/ProductDetail';
 import React from 'react';
-// import LoginPage from './login/page';
+import SignupPage from './signup/page';
+import LoginPage from './login/page';
 
 export default function Home() {
   return (
     <div>
-      {/* <MainPage />
+      <MainPage />
       <Subcategory />
       <Card
         title="Ghost, Ghouls and Gallows Walking Tour with Boat Ride"
         price="100000"
         rating="4"
-      /> */}
-      <ProductDetail />
-      {/* <LoginPage /> */}
+      /> 
+      {/* <ProductDetail /> */}
+      <LoginPage />
+      <SignupPage />
     </div>
   );
 }
