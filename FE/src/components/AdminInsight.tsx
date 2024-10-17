bf86349147a4cad463cc7df8ec695929f2098e52
ksubi666 <Ksubi666@gmail.com>
2024-10-17 12:34:53 +0800

    [FE] refactor Insight order

--- FE/src/components/AdminInsight.tsx
+++ FE/src/components/AdminInsight.tsx
@@ -6,8 +6,8 @@ import InsightOrderTitles from './InsightOrderTitles';
 
 const AdminInsight = () => {
   return (
-    <div className="w-full h-full p-5 flex flex-col gap-5">
-      <div className="flex gap-5 h-full">
+    <div className="w-full h-screen p-5 flex flex-col gap-6">
+      <div className="flex gap-5 h-fit">
         <div className="grid grid-rows-3 min-h-[150px] w-[calc(50%)] gap-5">
           <InsightCard title="Order" count="100" />
           <InsightCard title="Review" count="200" />
@@ -17,7 +17,7 @@ const AdminInsight = () => {
           <InsightChart />
         </div>
       </div>
-      <div className="border-[1px] rounded-lg flex flex-col gap-1 overflow-y-auto">
+      <div className="border-[1px] h-full rounded-lg flex flex-col gap-1 overflow-y-auto">
         <InsightOrderTitles />
         <InsightOrders />
         <InsightOrders />
