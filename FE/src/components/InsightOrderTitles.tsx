66d8076819d0645396e78f8cf65f14c35138b2d1
ksubi666 <Ksubi666@gmail.com>
2024-10-17 12:23:01 +0800

    [FE] Insight order list UI

---
+++ FE/src/components/InsightOrderTitles.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { styles } from './InsightOrders';
+
+const InsightOrderTitles = () => {
+  return (
+    <div className={styles.titleContainer}>
+      <div className={styles.subContainer}>Order number</div>
+      <div className={styles.subContainer}>User info</div>
+      <div className={styles.subContainer}>tovlogdson udur</div>
+      <div className="flex pl-8 items-center">status</div>
+      <div className={styles.subContainer}>hudaldaj avsn date</div>
+    </div>
+  );
+};
+
+export default InsightOrderTitles;
