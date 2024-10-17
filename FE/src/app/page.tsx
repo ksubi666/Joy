e7e16d0248c2695514d6ad0b059330728d14c85b
Sureneee <soyolsurenbs@gmail.com>
2024-10-17 16:49:27 +0800

    [FE] Wishlist ui

--- FE/src/app/page.tsx
+++ FE/src/app/page.tsx
@@ -5,20 +5,29 @@ import ProductDetail from '@/components/ProductDetail';
 import React from 'react';
 import SignupPage from './signup/page';
 import LoginPage from './login/page';
+import Wishlist from './wishlist/page';
 
 export default function Home() {
   return (
     <div>
-      <MainPage />
+      {/* <MainPage />
       <Subcategory />
       <Card
         title="Ghost, Ghouls and Gallows Walking Tour with Boat Ride"
         price="100000"
         rating="4"
-      /> 
+      />  */}
       {/* <ProductDetail /> */}
-      <LoginPage />
-      <SignupPage />
+      {/* <LoginPage />
+      <SignupPage /> */}
+      <Wishlist 
+      category="Adventure"
+      title="Ghost, Ghouls and Gallows Walking Tour with Boat Ride"
+      time="2 цаг"
+      rating="5"
+      price="657,800₮"
+      count={999}
+      />
     </div>
   );
 }
