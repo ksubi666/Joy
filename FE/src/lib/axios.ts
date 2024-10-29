c13b8637406e718ecbdaa3938ecd3c3e9f2d481c
ksubi666 <Ksubi666@gmail.com>
2024-10-29 20:30:29 +0800

    deploy test

--- FE/src/lib/axios.ts
+++ FE/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 export const axiosInstance: AxiosInstance = axios.create({
-  baseURL: 'https://joy-backend.vercel.app/',
+  baseURL: 'http://localhost:8000',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
