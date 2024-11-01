cc5ee5d960058fc74f12c3705025182d149a273b
ksubi666 <Ksubi666@gmail.com>
2024-11-01 15:16:35 +0800

    env fix

--- FE/src/lib/axios.ts
+++ FE/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 export const axiosInstance: AxiosInstance = axios.create({
-  baseURL: process.env.PORT,
+  baseURL: process.env.NEXT_PUBLIC_PORT,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
